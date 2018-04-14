cc6197bb4ec7e675212b629ee01d5de8aa0f828d
jingling.long <https://mail.aorise.org/#5>
2018-04-15 00:41:27 +0800

    just save

--- src/main/java/org/ljl/look/CloudApplication.java
+++ src/main/java/org/ljl/look/CloudApplication.java
@@ -3,11 +3,12 @@ package org.ljl.look;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableTransactionManagement
-//@EnableEurekaClient
+@EnableEurekaClient
 @MapperScan("org.ljl.look.cloud.mapper")
 public class CloudApplication {
     public static void main(String[] args) {
